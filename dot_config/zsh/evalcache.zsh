435b5396b978fde84718710b2a7cd60d5b2687ee
Jacquin Moon <jq.nhi2306@gmail.com>
2024-08-07 09:49:52 +0700

    evalcache fix

--- dot_config/zsh/evalcache.zsh
+++ dot_config/zsh/evalcache.zsh
@@ -1,16 +1,14 @@
-ex
-
 if command -v starship >/dev/null; then
   _evalcache starship init zsh
 fi
 
 if command -v pyenv >/dev/null; then
   _evalcache pyenv init - --no-rehash zsh
-  _evalcache pyenv virtualenv-init -
+  _evalcache pyenv virtualenv-init - zsh
 fi
 
 if command -v rbenv >/dev/null; then
   _evalcache rbenv init - --no-rehash zsh
 fi
 
-alias evalcclear='_evalcache_clear'
+alias evalclear='_evalcache_clear'
