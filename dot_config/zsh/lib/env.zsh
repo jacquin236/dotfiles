bc4ebcfe13a669511353532a8f3ac6958ac39505
Jacquin Moon <jq.nhi2306@gmail.com>
2024-08-09 05:47:52 +0700

    fix

--- dot_config/zsh/lib/env.zsh
+++ dot_config/zsh/lib/env.zsh
@@ -10,7 +10,7 @@ export XDG_PROJECTS_DIR=${XDG_PROJECTS_DIR:-$HOME/projects}
 
 # zsh
 export ZDOTDIR=${ZDOTDIR:-$XDG_CONFIG_HOME/zsh}
-export ZSH_COMPDUMP=$XDG_CACHE_HOME/zsh/compdump/zcompdump*
+export ZSH_COMPDUMP=$XDG_CACHE_HOME/zsh/compdump/zcompdump$SHORT_HOST-$VERSION
 
 # WSL Utilities
 if [ "$(uname -r | sed -n 's/.*\( *Microsoft *\).*/\1/ip')" ] || command -v wslview >/dev/null; then
